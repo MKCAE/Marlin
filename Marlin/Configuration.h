d65de20c14a2cf7fb818735900ddab0cbcdd8743
MKCAE <66804659+MKCAE@users.noreply.github.com>
2020-06-15 21:22:48 +0200

    kolejna korekcja e steps i eeprom

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -609,7 +609,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.058, 80.058, 399.2901, 985.62 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.058, 80.058, 399.2901, 415 }
 
 /**
  * Default Max Feed Rate (mm/s)
